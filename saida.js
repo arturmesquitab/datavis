4de1a55203e52f055c296b24d13a383155c661d5
Artur <arturmesquitab@gmail.com>
2016-04-20 15:29:58 -0300

    Add to page

---
+++ saida.js
@@ -0,0 +1,5 @@
+var x = [ 'The Dark Knight', 'Up', 'Ratatouille', 'WALL-E', 'The Kings Speech', 'Slumdog Millionaire', 'Black Swan', 'Inglourious Basterds', 'The Blind Side', 'Juno', 'The Help', 'No Country for Old Men', 'Midnight in Paris', 'The Fighter', 'The Artist', 'Michael Clayton', 'There Will Be Blood', 'Milk', 'The Hurt Locker', 'Beginners'];
+var y1 = [ 999, 731, 621, 521, 0, 0, 329, 321, 309, 231, 206, 0, 149, 0, 0, 93, 76, 0, 0, 14];
+var y2 = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 0, 0, 0, 50, 0, 0];
+var y3 = [ 0, 0, 0, 0, 0, 376, 0, 0, 0, 0, 0, 0, 0, 0, 106, 0, 0, 0, 49, 0];
+var y4 = [ 0, 0, 0, 0, 427, 0, 0, 0, 0, 0, 0, 161, 0, 0, 0, 0, 0, 0, 0, 0];
